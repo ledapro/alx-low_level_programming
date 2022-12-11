407bae25eecd1ae0b400cbdfcf3e0010239be866
kent <kentudosen@gmail.com>
2022-12-11 10:06:21 -0800

    dxd

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,9 +8,6 @@
  */
 int main(void)
 {
-	char *mgs = "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n";
-
-	write(STDOUT_FILENO, mgs, 59);
-
+	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
