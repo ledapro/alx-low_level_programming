e7abdabb2a460637f87e84da8c3435e9a7838d35
kent <kentudosen@gmail.com>
2022-12-11 08:31:04 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *
+ *  *main - entry point
+ *  *
+ *  *Return: 0 after printing the function
+ *  *
+ *  */
+
+int main(void)
+
+{puts("\"Programming is like building a multilingual puzzle");
+
+		return (0);
+
+}
