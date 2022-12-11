3d3b5521335f4cfb87d197857be5d67aeefd2a00
kent <kentudosen@gmail.com>
2022-12-11 08:42:43 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * *main - entry point
+ * main - Entry Point
  *
- * *Return: 0 after printing the function
- * */
+ * Return: 0 after printing the function
+ */
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle");
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
