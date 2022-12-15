24e5f28508d3d682e0114a9aa21e90cbfd5b794f
kent <kentudosen@gmail.com>
2022-12-15 14:04:54 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
 /**
- * print_numbers - print 0123456789
- * owned by Bwave ICT
- * Return: void
+ * print_numbers - prints number
 */
 
 void print_numbers(void)
 {
-	char c;
+	int i;
 
-	for (c = '0'; c <= '9'; c++)
+	for (i = 48; i < 58; i++)
 	{
-		_putchar(c);
+		_putchar(i);
 	}
 	_putchar('\n');
 }
